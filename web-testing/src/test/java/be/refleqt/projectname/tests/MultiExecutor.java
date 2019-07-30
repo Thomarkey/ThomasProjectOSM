daeac72faecde906695c18007ecfc0680d542c53
miranro <robbert.mirandolle@refleqt.be>
2019-07-30 15:59:50 +0200

    Add multiBrowser Support

--- web-testing/src/test/java/be/refleqt/projectname/tests/MultiExecutor.java
+++ web-testing/src/test/java/be/refleqt/projectname/tests/MultiExecutor.java
@@ -1,21 +1,24 @@
 package be.refleqt.projectname.tests;
 
+import be.refleqt.library.selenium.DriverProvider;
 import be.refleqt.library.selenium.driver.setup.DockerProvider;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class MultiExecutor {
 
     @Test(description = "Runs Cucumber Feature")
-    public void executeTest() {
+    @Parameters({"browser"})
+    public void executeTest(@Optional String browser) {
         String tag = System.getProperty("cucumberTag", "wip");
-        System.out.println("Running tag: " + tag);
-        String arg = "src/test/resources/features/ --threads " + System.getProperty("threads", "4") +
+        String arg = "src/test/resources/features/ --threads " + System.getProperty("threads", "2") +
                 " --plugin json:target/cucumber-report/test.json " +
                 "--plugin html:target/cucumber-report/html " +
                 "-t @" + tag + " --strict --glue be.refleqt.projectname.steps";
         String[] args = arg.split(" ");
+
+        if (browser != null) {
+            DriverProvider.setBrowser(browser);
+        }
 
         cucumber.api.cli.Main.run(args, Thread.currentThread().getContextClassLoader());
     }
@@ -25,7 +28,7 @@ public class MultiExecutor {
      */
     @BeforeSuite
     public static void setupDocker() {
-        DockerProvider.getInstance().setupDockerGrid(System.getProperty("threads", "4"));
+        DockerProvider.getInstance().setupDockerGrid(System.getProperty("threads", "2"));
     }
 
     @AfterSuite
