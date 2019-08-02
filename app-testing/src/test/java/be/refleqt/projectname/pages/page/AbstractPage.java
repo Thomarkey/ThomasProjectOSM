160425bdd546ef7ae4c39a628a47c92fc97f1e31
miranro <robbert.mirandolle@refleqt.be>
2019-08-02 16:26:56 +0200

    Add Appium Support - READme still TODO

---
+++ app-testing/src/test/java/be/refleqt/projectname/pages/page/AbstractPage.java
@@ -0,0 +1,10 @@
+package be.refleqt.projectname.pages.page;
+
+import be.refleqt.library.appium.GenericAbstractPage;
+
+public class AbstractPage extends GenericAbstractPage {
+
+    public AbstractPage() {
+        super();
+    }
+}
