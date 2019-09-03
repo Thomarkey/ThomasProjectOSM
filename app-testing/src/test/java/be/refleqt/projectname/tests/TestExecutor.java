df67060aab002e830b784302b5806b1504d0c750
miranro <robbert.mirandolle@refleqt.be>
2019-09-03 16:35:23 +0200

    Remove the usage of testSuite.xml + python script to generate them.

--- app-testing/src/test/java/be/refleqt/projectname/tests/TestExecutor.java
+++ app-testing/src/test/java/be/refleqt/projectname/tests/TestExecutor.java
@@ -1,15 +1,19 @@
 package be.refleqt.projectname.tests;
 
+import be.refleqt.library.appium.CommonDataProvider;
 import be.refleqt.library.appium.DriverProvider;
 import org.testng.ITestResult;
 import org.testng.Reporter;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class TestExecutor {
 
-    @Test(description = "Runs Cucumber Feature")
-    @Parameters({"device"})
-    public void executeTest(@Optional String device) {
+    @Test(description = "Runs Cucumber Feature",
+            dataProvider = "getDevices",
+            dataProviderClass = CommonDataProvider.class)
+    public void executeTest(String device) {
         String tag = System.getProperty("cucumberTag", "wip");
         System.out.println("Running tag: " + tag);
         String arg = "src/test/resources/features/ --threads " + System.getProperty("threads", "1") +
@@ -18,9 +22,7 @@ public class TestExecutor {
                 "-t @" + tag + " --strict --glue be.refleqt.projectname.steps";
         String[] args = arg.split(" ");
 
-        if (device != null) {
-            DriverProvider.setDevice(device);
-        }
+        DriverProvider.setDevice(device);
 
         byte exitCode = io.cucumber.core.cli.Main.run(args, Thread.currentThread().getContextClassLoader());
 
@@ -34,7 +36,7 @@ public class TestExecutor {
         DriverProvider.startAppiumServer();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public static void stopAppium() {
         DriverProvider.stopAppiumServer();
     }
