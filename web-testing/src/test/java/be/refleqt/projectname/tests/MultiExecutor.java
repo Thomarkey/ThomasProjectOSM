b56eb5191c5997800924ece2c95fe0d08c6805b9
miranro <robbert.mirandolle@refleqt.be>
2019-09-04 14:58:40 +0200

    Make MultiExecutor use the dataProvider for multiple browser support

--- web-testing/src/test/java/be/refleqt/projectname/tests/MultiExecutor.java
+++ web-testing/src/test/java/be/refleqt/projectname/tests/MultiExecutor.java
@@ -1,26 +1,27 @@
 package be.refleqt.projectname.tests;
 
+import be.refleqt.library.selenium.CommonDataProvider;
 import be.refleqt.library.selenium.DriverProvider;
 import be.refleqt.library.selenium.driver.setup.DockerProvider;
 import org.testng.ITestResult;
 import org.testng.Reporter;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class MultiExecutor {
 
-    @Test(description = "Runs Cucumber Feature")
-    @Parameters({"browser"})
-    public void executeTest(@Optional String browser) {
+    @Test(description = "Runs Cucumber Feature",
+            dataProvider = "getBrowsers",
+            dataProviderClass = CommonDataProvider.class)
+    public void executeTest(String browser) {
         String tag = System.getProperty("cucumberTag", "wip");
         String arg = "src/test/resources/features/ --threads " + System.getProperty("threads", "2") +
                 " --plugin json:target/cucumber-report/test.json " +
                 "--plugin html:target/cucumber-report/html " +
                 "-t @" + tag + " --strict --glue be.refleqt.projectname.steps";
         String[] args = arg.split(" ");
-
-        if (browser != null) {
-            DriverProvider.setBrowser(browser);
-        }
+        DriverProvider.setBrowser(browser);
 
         byte exitCode = io.cucumber.core.cli.Main.run(args, Thread.currentThread().getContextClassLoader());
 
